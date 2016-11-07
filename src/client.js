15cd20a8ae475fab3c3b68b62416bfad109d6ddd
rustamse <rustamse@outlook.com>
2016-11-07 17:46:19 +0500

    Добавил тесты на покупку пиццы

---
+++ src/client.js
@@ -0,0 +1,37 @@
+'use strict';
+
+export class Client {
+    constructor(pizza, payAmount) {
+        this.pizza = pizza;
+        this.payAmount = payAmount;
+    }
+
+    buyPizza() {
+        let pizzaPrice = this.pizza.calcPrice();
+
+        return this.payAmount >= pizzaPrice;
+    }
+}
+
+export class ClientBuilder {
+    constructor() {
+        this.state = {
+            pizza: null,
+            payAmount: 0
+        };
+    }
+
+    whenOrderPizza(pizza) {
+        this.state.pizza = pizza;
+        return this;
+    }
+
+    whenPayForPizza(payAmount) {
+        this.state.payAmount = payAmount;
+        return this;
+    }
+
+    build() {
+        return new Client(this.state.pizza, this.state.payAmount);
+    }
+}
---
+++ test/clientTests.js
@@ -0,0 +1,40 @@
+import assert from 'assert'
+import sinon from 'sinon'
+import {Client, ClientBuilder} from '../src/client'
+
+suite('Client buy pizza tests', function () {
+
+    suite('when pizza price is 300', function () {
+        let pizzaStub = {
+            calcPrice: function () {
+                return 300;
+            }
+        };
+
+        suite('when pay amount is 400', function () {
+            test('will get pizza', function () {
+                let client = new ClientBuilder()
+                    .whenOrderPizza(pizzaStub)
+                    .whenPayForPizza(400)
+                    .build();
+
+                var isSuccess = client.buyPizza();
+
+                assert.equal(true, isSuccess);
+            });
+        });
+
+        suite('when pay amount is 200', function () {
+            test('will NOT get pizza', function () {
+                let client = new ClientBuilder()
+                    .whenOrderPizza(pizzaStub)
+                    .whenPayForPizza(200)
+                    .build();
+
+                var isSuccess = client.buyPizza();
+
+                assert.equal(false, isSuccess);
+            });
+        });
+    });
+});
